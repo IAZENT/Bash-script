2e8b5d1c5f3b79819bbc0ec23e66ef0a1cf278ca
IAZENT <106596735+IAZENT@users.noreply.github.com>
2024-02-27 09:23:00 +0545

    Update enc-send-A.sh

--- enc-send-A.sh
+++ enc-send-A.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-
+#Generate public and private key
 openssl genrsa -out entityA_private.pem 2048
 openssl rsa -pubout -in entityA_private.pem -out entityA_public.pem
 
+#Generate symmetric key and a file with data
 symmetric_key=$(openssl rand -hex 16)
 echo "Some sensitive data" > data.txt
 
-scp entityA_public.pem kali@192.168.18.240:/home/kali/tasks
-scp entityA_private.pem kali@192.168.18.240:/home/kali/tasks
+#send public and private key
+scp entityA_public.pem username@ip:/path/to/destination
+scp entityA_private.pem username@ip:/path/to/destination
 
+#Generate digital signature
 openssl dgst -sha256 -sign entityA_private.pem -out signature_entity1.sha256 data.txt
 
-
+#Encryption
 openssl enc -pbkdf2 -aes-256-cbc -in data.txt -out encrypted_data.enc -k $symmetric_key
 
 openssl pkeyutl -encrypt -pubin -inkey entityA_public.pem -in <(echo -n "$symmetric_key") -out encrypted_symmetric_key.bin
 
-
-scp encrypted_data.enc kali@192.168.18.240:/home/kali/tasks
-scp encrypted_symmetric_key.bin kali@192.168.18.240:/home/kali/tasks
+#Send encrypted file 
+scp encrypted_data.enc username@ip:/path/to/destination
+scp encrypted_symmetric_key.bin username@ip:/path/to/destination
 
